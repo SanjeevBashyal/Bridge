b67252be2de16dbcfec22d0f5fe83004f82a3df7
Sanjeev Bashyal <sanjeev.bashyal01@gmail.com>
2021-06-16 22:13:01 +0545

    migrates bridge design to bridge repo

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+// import HeaderClass from './Components/HeaderClass'
+import Header from './Components/Header'
+import BridgeInput from './Components/BridgeInput'
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <BridgeInput/>
+    </div>
+  );
+}
+
+export default App;
